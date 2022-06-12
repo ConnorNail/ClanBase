6256f3d3a86bd3b5f2e967b69be6f778feb845ca
manff <nailboyz0247@cox.net>
2022-06-11 17:00:37 -0700

    Working total activity tracker

--- src/pages/[clanid]/members-test/index.js
+++ src/pages/[clanid]/members-test/index.js
@@ -11,14 +11,16 @@ export default function TestTable({ clanid }) {
     // an alternative hook based API
     const [css] = useStyletron()
 
+    const [data, setData] = useState([]);
+
     useEffect(() => {
         const url = 'https://www.bungie.net/Platform/GroupV2/' + clanid + '/Members/';
 
         const current = new Date()
-        const startDate = current.getFullYear() + '/' + ('0' + (current.getMonth()+1)).slice(-2) + '/' + ('0' + current.getDate()).slice(-2)
+        const startDate = current.getFullYear() + '/' + ('0' + (current.getMonth() + 1)).slice(-2) + '/' + ('0' + current.getDate()).slice(-2)
         const past = new Date()
         past.setDate(past.getDate() - 31)
-        const endDate = past.getFullYear() + '/' + ('0' + (past.getMonth()+1)).slice(-2) + '/' + ('0' + past.getDate()).slice(-2)
+        const endDate = past.getFullYear() + '/' + ('0' + (past.getMonth() + 1)).slice(-2) + '/' + ('0' + past.getDate()).slice(-2)
 
         async function getData() {
             const out = fetch(url, { headers })
@@ -28,7 +30,7 @@ export default function TestTable({ clanid }) {
                     return data.Response.results
                 })
                 .then(async (data) => {
-                    await Promise.all([
+                    await Promise.all(
                         data.map((res, i, array) => {
                             return fetch('https://www.bungie.net/Platform/Destiny2/' + res.destinyUserInfo.membershipType + '/Account/' + res.destinyUserInfo.membershipId + '/Stats/', { headers })
                                 .then((res) => res.json())
@@ -39,37 +41,55 @@ export default function TestTable({ clanid }) {
                                         .then(async (data) => {
                                             const char1 = await fetch('https://www.bungie.net/Platform/Destiny2/' + res.destinyUserInfo.membershipType + '/Account/' + res.destinyUserInfo.membershipId + '/Character/' + data.Response.profile.data.characterIds[0] + '/Stats/?dayend=' + startDate + '&daystart=' + endDate + '&periodType=Daily', { headers })
                                                 .then((res) => res.json())
-                                                .then((data) => {return{playerTimeChar1: data.Response}})
+                                                .then((data) => { return { playerTimeChar1: data.Response } })
+                                                .catch((error) => {
+                                                    console.error('Error:', error);
+                                                })
                                             if (data.Response.profile.data.characterIds.length > 1) {
                                                 const char2 = await fetch('https://www.bungie.net/Platform/Destiny2/' + res.destinyUserInfo.membershipType + '/Account/' + res.destinyUserInfo.membershipId + '/Character/' + data.Response.profile.data.characterIds[1] + '/Stats/?dayend=' + startDate + '&daystart=' + endDate + '&periodType=Daily', { headers })
                                                     .then((res) => res.json())
-                                                    .then((data) => {return{playerTimeChar2: data.Response}})
+                                                    .then((data) => { return { playerTimeChar2: data.Response } })
+                                                    .catch((error) => {
+                                                        console.error('Error:', error);
+                                                    })
                                                 if (data.Response.profile.data.characterIds.length > 2) {
                                                     const char3 = await fetch('https://www.bungie.net/Platform/Destiny2/' + res.destinyUserInfo.membershipType + '/Account/' + res.destinyUserInfo.membershipId + '/Character/' + data.Response.profile.data.characterIds[2] + '/Stats/?dayend=' + startDate + '&daystart=' + endDate + '&periodType=Daily', { headers })
                                                         .then((res) => res.json())
-                                                        .then((data) => {return{playerTimeChar3: data.Response}})
-                                                        return {...char1, ...char2, ...char3}
+                                                        .then((data) => { return { playerTimeChar3: data.Response } })
+                                                        .catch((error) => {
+                                                            console.error('Error:', error);
+                                                        })
+                                                    return combineTime({ ...char1, ...char2, ...char3 })
                                                 }
-                                                return {...char1, ...char2}
+                                                return combineTime({ ...char1, ...char2 })
                                             } else {
-                                                return char1
+                                                return combineTime(char1)
                                             }
+                                        })
+                                        .catch((error) => {
+                                            console.error('Error:', error);
                                         })
 
                                     array[i] = { ...res, ...data, ...playerTime }
                                     console.log("update stats")
                                 })
+                                .catch((error) => {
+                                    console.error('Error:', error);
+                                })
                         })
-                    ])
-                    .then((res) => console.log(res))
+                    )
                     console.log("new", data)
+                    setData(data)
+                })
+                .catch((error) => {
+                    console.error('Error:', error);
                 })
 
             console.log(await out)
         }
         getData()
 
-        function combineTime(char1TimeStats) {
+        function combineTime(timeStats) {
             let time = {
                 allPvE: 0,
                 allPvECompetitive: 0,
@@ -77,25 +97,140 @@ export default function TestTable({ clanid }) {
                 allStrikes: 0,
                 patrol: 0,
                 raid: 0,
-                story: 0
+                story: 0,
+                total: 0,
             }
-            
-            Object.keys(char1TimeStats).forEach((activity, i) => {
-                const temp = char1TimeStats[activity].daily
-                const name = Object.keys(activity)
-    
-                if (typeof temp != 'undefined') {
-                    for (let i = 0; i < temp.length; i++) {
-                        time[activity] = time[activity] + temp[i].values.totalActivityDurationSeconds.basic.value
+
+            Object.keys(timeStats).forEach((character) => {
+                Object.keys(timeStats[character]).forEach((activity) => {
+                    const daily = timeStats[character][activity].daily
+
+                    if (typeof daily != 'undefined') {
+                        for (let i = 0; i < daily.length; i++) {
+                            time[activity] = time[activity] + daily[i].values.totalActivityDurationSeconds.basic.value
+                        }
                     }
-                } else {
-                    time[activity] = 0
-                }
+                })
             })
-            return time
+
+            time.total = time.allPvE + time.allPvP
+            return { playerTime: time }
         }
 
     }, [])
+
+    function getDisplayTime(seconds) {
+        return Math.trunc(seconds / 86400) + 'd ' + Math.trunc((seconds % 86400) / 3600) + 'hr ' + Math.trunc((seconds % 3600) / 60) + 'm ' + seconds % 60 + 's'
+    }
+
+    const columns = useMemo(
+        () => [
+            {
+                Header: "Info",
+                columns: [
+                    {
+                        Header: "Emblem",
+                        accessor: "bungieNetUserInfo.iconPath",
+                        Cell: ({ cell: { value } }) => {
+                            return (
+                                <>
+                                    <Image maxW="3rem" src={'https://www.bungie.net/' + value} />
+                                </>
+                            )
+                        }
+                    },
+                    {
+                        Header: "Name",
+                        accessor: "destinyUserInfo.displayName"
+                    },
+                    {
+                        Header: "User ID",
+                        accessor: "destinyUserInfo.membershipId"
+                    },
+                    {
+                        Header: "Join Date",
+                        accessor: "joinDate"
+                    },
+                    {
+                        Header: "Original Platform",
+                        accessor: "destinyUserInfo.membershipType",
+                        Cell: ({ cell: { value } }) => {
+                            return (
+                                <>
+                                    {value == 1 ? 'Xbox' : value == 2 ? 'Playstation' : value == 3 ? 'Steam' : value == 4 ? 'Blizzard' : value == 5 ? 'Stadia' : 'error'}
+                                </>
+                            )
+                        }
+                    },
+                    {
+                        Header: "Online",
+                        accessor: "isOnline",
+                        Cell: ({ cell: { value } }) => {
+                            return (
+                                <Div bg={value ? "success600" : "danger600"} rounded="xl" >
+                                    <Text textSize="body" m={{ l: "0.5rem", r: "0.5rem" }}>
+                                        {value ? 'Online' : 'Offline'}
+                                    </Text>
+                                </Div>
+                            )
+                        }
+                    },
+                ]
+            },
+            {
+                Header: "Stats",
+                columns: [
+                    {
+                        Header: "KD",
+                        accessor: "playerStats.allPvP.allTime.killsDeathsRatio.basic.displayValue",
+                    },
+                    {
+                        Header: "Combat Rating",
+                        accessor: "playerStats.allPvP.allTime.combatRating.basic.displayValue",
+                    },
+                ]
+            },
+            {
+                Header: "Activity in the last 31 days",
+                columns: [
+                    {
+                        Header: "Time in PvP",
+                        accessor: "playerTime.allPvP",
+                        Cell: ({ cell: { value } }) => {
+                            return (
+                                <Div>
+                                    {getDisplayTime(value)}
+                                </Div>
+                            )
+                        }
+                    },
+                    {
+                        Header: "Time in PvE",
+                        accessor: "playerTime.allPvE",
+                        Cell: ({ cell: { value } }) => {
+                            return (
+                                <Div>
+                                    {getDisplayTime(value)}
+                                </Div>
+                            )
+                        }
+                    },
+                    {
+                        Header: "Total Time",
+                        accessor: "playerTime.total",
+                        Cell: ({ cell: { value } }) => {
+                            return (
+                                <Div>
+                                    {getDisplayTime(value)}
+                                </Div>
+                            )
+                        }
+                    },
+                ]
+            }
+        ],
+        []
+    );
 
     return (
         <DefaultTemplate>
@@ -105,7 +240,7 @@ export default function TestTable({ clanid }) {
                 </Text>
             </Div>
             <Div>
-
+                <Table columns={columns} data={data} />
             </Div>
         </DefaultTemplate>
     )
