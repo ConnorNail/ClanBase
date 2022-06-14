d897dbf5544830945e343783337521678e4b7b7c
manff <nailboyz0247@cox.net>
2022-06-13 20:27:38 -0700

    Working members page

--- src/pages/[clanid]/members/index.js
+++ src/pages/[clanid]/members/index.js
@@ -7,16 +7,122 @@ import { Div, Text, Image } from "atomize";
 const apikey = '3a85f7e1a4444ec1865efb39ef019313';
 const headers = { 'X-API-Key': apikey }
 
-export default function TestTable({ data, playerTime }) {
+export default function TestTable({ clanid }) {
     // an alternative hook based API
     const [css] = useStyletron()
 
-    console.log(playerTime)
+    const [data, setData] = useState([]);
 
-    function getTimePlayed() {
-        
+    useEffect(() => {
+        const url = 'https://www.bungie.net/Platform/GroupV2/' + clanid + '/Members/';
+
+        const current = new Date()
+        const startDate = current.getFullYear() + '/' + ('0' + (current.getMonth() + 1)).slice(-2) + '/' + ('0' + current.getDate()).slice(-2)
+        const past = new Date()
+        past.setDate(past.getDate() - 31)
+        const endDate = past.getFullYear() + '/' + ('0' + (past.getMonth() + 1)).slice(-2) + '/' + ('0' + past.getDate()).slice(-2)
+
+        async function getData() {
+            const out = fetch(url, { headers })
+                .then((res) => res.json())
+                .then((data) => {
+                    console.log("old", data.Response.results)
+                    return data.Response.results
+                })
+                .then(async (data) => {
+                    await Promise.all(
+                        data.map((res, i, array) => {
+                            return fetch('https://www.bungie.net/Platform/Destiny2/' + res.destinyUserInfo.membershipType + '/Account/' + res.destinyUserInfo.membershipId + '/Stats/', { headers })
+                                .then((res) => res.json())
+                                .then((data) => { return { playerStats: data.Response.mergedAllCharacters.results } })
+                                .then(async (data) => {
+                                    const playerTime = await fetch('https://www.bungie.net/Platform/Destiny2/' + res.destinyUserInfo.membershipType + '/Profile/' + res.destinyUserInfo.membershipId + '/?components=100', { headers })
+                                        .then((res) => res.json())
+                                        .then(async (data) => {
+                                            const char1 = await fetch('https://www.bungie.net/Platform/Destiny2/' + res.destinyUserInfo.membershipType + '/Account/' + res.destinyUserInfo.membershipId + '/Character/' + data.Response.profile.data.characterIds[0] + '/Stats/?dayend=' + startDate + '&daystart=' + endDate + '&periodType=Daily', { headers })
+                                                .then((res) => res.json())
+                                                .then((data) => { return { playerTimeChar1: data.Response } })
+                                                .catch((error) => {
+                                                    console.error('Error:', error);
+                                                })
+                                            if (data.Response.profile.data.characterIds.length > 1) {
+                                                const char2 = await fetch('https://www.bungie.net/Platform/Destiny2/' + res.destinyUserInfo.membershipType + '/Account/' + res.destinyUserInfo.membershipId + '/Character/' + data.Response.profile.data.characterIds[1] + '/Stats/?dayend=' + startDate + '&daystart=' + endDate + '&periodType=Daily', { headers })
+                                                    .then((res) => res.json())
+                                                    .then((data) => { return { playerTimeChar2: data.Response } })
+                                                    .catch((error) => {
+                                                        console.error('Error:', error);
+                                                    })
+                                                if (data.Response.profile.data.characterIds.length > 2) {
+                                                    const char3 = await fetch('https://www.bungie.net/Platform/Destiny2/' + res.destinyUserInfo.membershipType + '/Account/' + res.destinyUserInfo.membershipId + '/Character/' + data.Response.profile.data.characterIds[2] + '/Stats/?dayend=' + startDate + '&daystart=' + endDate + '&periodType=Daily', { headers })
+                                                        .then((res) => res.json())
+                                                        .then((data) => { return { playerTimeChar3: data.Response } })
+                                                        .catch((error) => {
+                                                            console.error('Error:', error);
+                                                        })
+                                                    return combineTime({ ...char1, ...char2, ...char3 })
+                                                }
+                                                return combineTime({ ...char1, ...char2 })
+                                            } else {
+                                                return combineTime(char1)
+                                            }
+                                        })
+                                        .catch((error) => {
+                                            console.error('Error:', error);
+                                        })
+
+                                    array[i] = { ...res, ...data, ...playerTime }
+                                    console.log("update stats")
+                                })
+                                .catch((error) => {
+                                    console.error('Error:', error);
+                                })
+                        })
+                    )
+                    console.log("new", data)
+                    setData(data)
+                })
+                .catch((error) => {
+                    console.error('Error:', error);
+                })
+
+            console.log(await out)
+        }
+        getData()
+
+        function combineTime(timeStats) {
+            let time = {
+                allPvE: 0,
+                allPvECompetitive: 0,
+                allPvP: 0,
+                allStrikes: 0,
+                patrol: 0,
+                raid: 0,
+                story: 0,
+                total: 0,
+            }
+
+            Object.keys(timeStats).forEach((character) => {
+                Object.keys(timeStats[character]).forEach((activity) => {
+                    const daily = timeStats[character][activity].daily
+
+                    if (typeof daily != 'undefined') {
+                        for (let i = 0; i < daily.length; i++) {
+                            time[activity] = time[activity] + daily[i].values.totalActivityDurationSeconds.basic.value
+                        }
+                    }
+                })
+            })
+
+            time.total = time.allPvE + time.allPvP
+            return { playerTime: time }
+        }
+
+    }, [])
+
+    function getDisplayTime(seconds) {
+        return Math.trunc(seconds / 86400) + 'd ' + Math.trunc((seconds % 86400) / 3600) + 'hr ' + Math.trunc((seconds % 3600) / 60) + 'm ' + seconds % 60 + 's'
     }
-    
+
     const columns = useMemo(
         () => [
             {
@@ -83,6 +189,44 @@ export default function TestTable({ data, playerTime }) {
                         accessor: "playerStats.allPvP.allTime.combatRating.basic.displayValue",
                     },
                 ]
+            },
+            {
+                Header: "Activity in the last 31 days",
+                columns: [
+                    {
+                        Header: "Time in PvP",
+                        accessor: "playerTime.allPvP",
+                        Cell: ({ cell: { value } }) => {
+                            return (
+                                <Div>
+                                    {getDisplayTime(value)}
+                                </Div>
+                            )
+                        }
+                    },
+                    {
+                        Header: "Time in PvE",
+                        accessor: "playerTime.allPvE",
+                        Cell: ({ cell: { value } }) => {
+                            return (
+                                <Div>
+                                    {getDisplayTime(value)}
+                                </Div>
+                            )
+                        }
+                    },
+                    {
+                        Header: "Total Time",
+                        accessor: "playerTime.total",
+                        Cell: ({ cell: { value } }) => {
+                            return (
+                                <Div>
+                                    {getDisplayTime(value)}
+                                </Div>
+                            )
+                        }
+                    },
+                ]
             }
         ],
         []
@@ -104,84 +248,10 @@ export default function TestTable({ data, playerTime }) {
 
 export async function getServerSideProps(context) {
     const { clanid } = context.query
-    const url1 = 'https://www.bungie.net/Platform/GroupV2/' + clanid + '/Members/' //House Fruit: 2084197 Fruit Snaccs: 4599535
-    
-    const current = new Date()
-    const startDate = current.getFullYear() + '/' + ('0' + (current.getMonth()+1)).slice(-2) + '/' + ('0' + current.getDate()).slice(-2)
-    const past = new Date()
-    past.setDate(past.getDate() - 31)
-    const endDate = past.getFullYear() + '/' + ('0' + (past.getMonth()+1)).slice(-2) + '/' + ('0' + past.getDate()).slice(-2)
-
-    // Clan details
-    const res1 = await fetch(url1, { headers })
-    const json1 = await res1.json()
-
-    const playerData = await Promise.all(
-        json1.Response.results.map((res) =>
-            fetch('https://www.bungie.net/Platform/Destiny2/' + res.destinyUserInfo.membershipType + '/Account/' + res.destinyUserInfo.membershipId + '/Stats/', { headers })
-                .then((res) => res.json())
-                .then((data) => {return{playerStats: data.Response.mergedAllCharacters.results}})
-        )
-    )
-
-    const playerTime = await Promise.all(
-        json1.Response.results.map((res, i) =>
-            fetch('https://www.bungie.net/Platform/Destiny2/' + res.destinyUserInfo.membershipType + '/Profile/' + res.destinyUserInfo.membershipId + '/?components=100', { headers })
-                .then((res) => res.json())
-                .then(async (data) => /*{
-                    await Promise.all(data.Response.profile.data.characterIds.map((characterId, i, array) => {
-                        return fetch('https://www.bungie.net/Platform/Destiny2/' + res.destinyUserInfo.membershipType + '/Account/' + res.destinyUserInfo.membershipId + '/Character/' + characterId[i] + '/Stats/?dayend=' + startDate + '&daystart=' + endDate + '&periodType=Daily', { headers })
-                        .then((res) => res.json())
-                        .then((data) => {
-                            array[i] = {...characterId, ...data}
-                        })
-                    }))
-                }*/{
-                    return fetch('https://www.bungie.net/Platform/Destiny2/' + res.destinyUserInfo.membershipType + '/Account/' + res.destinyUserInfo.membershipId + '/Character/' + data.Response.profile.data.characterIds[0] + '/Stats/?dayend=' + startDate + '&daystart=' + endDate + '&periodType=Daily', { headers })
-                        .then((res) => res.json())
-                        .then((data) => {return{playerTime: combineTime(data.Response)}})}
-                )
-        )
-    )
-
-    function combineTime(timeStats) {
-        let time = {
-            allPvE: 0,
-            allPvECompetitive: 0,
-            allPvP: 0,
-            allStrikes: 0,
-            patrol: 0,
-            raid: 0,
-            story: 0
-        }
-        //const activity = timeStats.allPvE
-        Object.keys(timeStats).forEach((activity, i) => {
-            const temp = timeStats[activity].daily
-            const name = Object.keys(activity)
-
-            if (typeof temp != 'undefined') {
-                for (let i = 0; i < temp.length; i++) {
-                    time[activity] = time[activity] + temp[i].values.totalActivityDurationSeconds.basic.value
-                }
-            } else {
-                time[activity] = 0
-            }
-        })
-        return time
-    }
-
-    function mergeArrayObjects(arr1, arr2, arr3) {
-        return arr1.map((item, i) => {
-            return Object.assign({}, item, arr2[i], arr3[i])
-        })
-    }
-
-    const merged = mergeArrayObjects(json1.Response.results, playerData, playerTime)
 
     return {
         props: {
-            data: merged,
-            playerTime: playerTime
+            clanid: clanid
         },
     }
 }
