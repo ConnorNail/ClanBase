d164325e857c39fd7f9a54a117bc345414a69c0d
manff <nailboyz0247@cox.net>
2022-07-11 17:12:37 -0700

    Update [...nextauth].js

--- src/pages/api/auth/[...nextauth].js
+++ src/pages/api/auth/[...nextauth].js
@@ -2,20 +2,34 @@ import NextAuth from "next-auth";
 import BungieProvider from "next-auth/providers/bungie";
 
 export default NextAuth({
-  // Configure one or more authentication providers
-  providers: [
-    BungieProvider({
-      clientId: process.env.BUNGIE_CLIENT_ID,
-      clientSecret: process.env.BUNGIE_SECRET,
-      headers: {
-        "X-API-Key": process.env.BUNGIE_API_KEY
-      },
-      authorization: {
-        url: 'https://www.bungie.net/en/OAuth/Authorize?client_id={'+process.env.BUNGIE_CLIENT_ID+'}&response_type=code',
-        params: {
-           scope: '',
+    // Configure one or more authentication providers
+    providers: [
+        BungieProvider({
+            clientId: process.env.BUNGIE_CLIENT_ID,
+            clientSecret: process.env.BUNGIE_SECRET,
+            headers: {
+                "X-API-Key": process.env.BUNGIE_API_KEY
+            },
+            authorization: {
+                url: 'https://www.bungie.net/en/OAuth/Authorize?client_id=' + process.env.BUNGIE_CLIENT_ID + '&response_type=code',
+                params: {
+                    scope: '',
+                },
+            }
+        }),
+    ],
+    logger: {
+        error(code, metadata) {
+            log.error(code, metadata)
         },
-     }
-    }),
-  ]
+        warn(code) {
+            log.warn(code)
+        },
+        debug(code, metadata) {
+            log.debug(code, metadata)
+        }
+    },
+    theme: {
+        colorScheme: "dark"
+    }
 })
