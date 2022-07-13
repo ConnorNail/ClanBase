ab9c60be8bf7629e84c6cc37f70530e415a2b8ad
manff <nailboyz0247@cox.net>
2022-07-12 22:44:27 -0700

    Debug

--- src/pages/login/index.js
+++ src/pages/login/index.js
@@ -2,17 +2,42 @@ import { styled, useStyletron } from 'styletron-react'
 import DefaultTemplate from '../../components/DefaultLayout'
 import LoginButton from '../../components/LoginButton'
 import { Row, Col, Div, Text, Image, Container, Anchor, Icon } from "atomize";
+import React, { useEffect } from "react";
 
-export default function Login( code, token) {
+export default function Login(code) {
     // an alternative hook based API
     const [css] = useStyletron()
 
-    console.log(token)
+
+    useEffect(() => {
+        async function getData() {
+            const token = await fetch('https://www.bungie.net/Platform/App/OAuth/Token/', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/x-www-form-urlencoded',
+                    'Authorization': 'Basic MzczMTY6RmxMU3F2MzdSeTNIaTR4NERpclRrM2dpc1FXQWNURmloZmlKSFQ2U1B0OA==',
+                    'X-API-Key': '3a85f7e1a4444ec1865efb39ef019313'
+                },
+                body: new URLSearchParams({
+                    'client_id': /*process.env.BUNGIE_CLIENT_ID*/ "37316",
+                    'grant_type': "authorization_code",
+                    'code': code
+                }).toString()
+            }).then(function (response) {
+                console.log("data", response);
+                return response.json();
+            });
+
+            console.log("test")
+        }
+        getData()
+
+    }, [])
 
     return (
         <DefaultTemplate>
             <Div h="100vh" bgImg="../destinybackground.png" bgPos="top" bgSize="auto" bgRepeat="no-repeat" m={{ l: "-0.5rem", r: "-0.5rem" }}>
-                <LoginButton/>
+                <LoginButton />
             </Div>
         </DefaultTemplate>
     )
@@ -23,27 +48,26 @@ export async function getServerSideProps(context) {
 
     const encodedString = Buffer.from(/*process.env.BUNGIE_CLIENT_ID*/ '37316' + ':' + 'FlLSqv37Ry3Hi4x4DirTk3gisQWAcTFihfiJHT6SPt8' /*process.env.BUNGIE_SECRET*/).toString('base64');
 
-    const token = await fetch('https://www.bungie.net/Platform/App/OAuth/Token/', {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/x-www-form-urlencoded',
-            'Authorization': 'Basic ' + encodedString,
-            'X-API-Key': '3a85f7e1a4444ec1865efb39ef019313'
-        },
-        body: new URLSearchParams({
-            'client_id': /*process.env.BUNGIE_CLIENT_ID*/ "37316",
-            'grant_type': "authorization_code",
-            'code': code
-        }).toString()
-    }).then(function (response) {
-        console.log(response);
-        return response.json();
-    });
+    // const token = await fetch('https://www.bungie.net/Platform/App/OAuth/Token/', {
+    //     method: 'POST',
+    //     headers: {
+    //         'Content-Type': 'application/x-www-form-urlencoded',
+    //         'Authorization': 'Basic MzczMTY6RmxMU3F2MzdSeTNIaTR4NERpclRrM2dpc1FXQWNURmloZmlKSFQ2U1B0OA==',
+    //         'X-API-Key': '3a85f7e1a4444ec1865efb39ef019313'
+    //     },
+    //     body: new URLSearchParams({
+    //         'client_id': /*process.env.BUNGIE_CLIENT_ID*/ "37316",
+    //         'grant_type': "authorization_code",
+    //         'code': code
+    //     }).toString()
+    // }).then(function (response) {
+    //     console.log("data", response);
+    //     return response.json();
+    // });
 
     return {
         props: {
-            code: code,
-            token: token
+            code: code
         }
     }
 }
