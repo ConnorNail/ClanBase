fc5eab01e70068b95a8ad4699149fcb6a052cdbf
manff <nailboyz0247@cox.net>
2022-07-14 21:33:17 -0700

    Update index.js

--- src/functions/refreshAccessToken/index.js
+++ src/functions/refreshAccessToken/index.js
@@ -1,33 +1,37 @@
+import React, { useState, useEffect } from "react";
+
 export default function refreshAccessToken() {
-    async function getData() {
-        const encodedString = Buffer.from(process.env.NEXT_PUBLIC_BUNGIE_CLIENT_ID + ':' + process.env.NEXT_PUBLIC_BUNGIE_SECRET).toString('base64');
+    useEffect(() => {
+        async function getData() {
+            const encodedString = Buffer.from(process.env.NEXT_PUBLIC_BUNGIE_CLIENT_ID + ':' + process.env.NEXT_PUBLIC_BUNGIE_SECRET).toString('base64');
 
-        const token = await fetch('https://www.bungie.net/Platform/App/OAuth/Token/', {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/x-www-form-urlencoded',
-                'Authorization': 'Basic ' + encodedString,
-                'X-API-Key': process.env.NEXT_PUBLIC_BUNGIE_API_KEY
-            },
-            body: new URLSearchParams({
-                'client_id': process.env.NEXT_PUBLIC_BUNGIE_CLIENT_ID,
-                'grant_type': "refresh_token",
-                'refresh_token': localStorage.getItem("refresh_token")
-            }).toString()
-        }).then(function (response) {
-            console.log("data", response);
-            return response.json();
-        }).catch((error) => {
-            console.error('Error:', error);
-            
-        });
+            const token = await fetch('https://www.bungie.net/Platform/App/OAuth/Token/', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/x-www-form-urlencoded',
+                    'Authorization': 'Basic ' + encodedString,
+                    'X-API-Key': process.env.NEXT_PUBLIC_BUNGIE_API_KEY
+                },
+                body: new URLSearchParams({
+                    'client_id': process.env.NEXT_PUBLIC_BUNGIE_CLIENT_ID,
+                    'grant_type': "refresh_token",
+                    'refresh_token': localStorage.getItem("refresh_token")
+                }).toString()
+            }).then(function (response) {
+                console.log("data", response);
+                return response.json();
+            }).catch((error) => {
+                console.error('Error:', error);
 
-        console.log("data", token)
-        localStorage.setItem("access_token", await token.access_token);
-        localStorage.setItem("refresh_token", await token.refresh_token);
-        localStorage.setItem("membership_is", await token.membership_id);
-    }
-    getData()
+            });
+
+            console.log("data", token)
+            localStorage.setItem("access_token", await token.access_token);
+            localStorage.setItem("refresh_token", await token.refresh_token);
+            localStorage.setItem("membership_is", await token.membership_id);
+        }
+        getData()
+    }, [])
 
     return true;
 }
