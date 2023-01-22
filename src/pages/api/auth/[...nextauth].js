a3bde3d09969a3e4a0b9ade03ae78cb7088ac905
Connor Nail <nailboyz0247@cox.net>
2023-01-22 01:16:51 -0700

    Small changes for Vercel

--- src/pages/api/auth/[...nextauth].js
+++ src/pages/api/auth/[...nextauth].js
@@ -5,6 +5,8 @@ async function refreshAccessToken(token) {
   console.log("Refreshing Access Token")
 
   try {
+    console.log(process.env.NEXT_PUBLIC_BUNGIE_API_KEY)
+
     const encodedString = 'MzczMTY6RmxMU3F2MzdSeTNIaTR4NERpclRrM2dpc1FXQWNURmloZmlKSFQ2U1B0OA=='//Buffer.from(process.env.NEXT_PUBLIC_BUNGIE_CLIENT_ID + ':' + process.env.NEXT_PUBLIC_BUNGIE_SECRET).toString('base64');
 
     const response = await fetch('https://www.bungie.net/Platform/App/OAuth/Token/', {
