0d4978eb5a49e098cba9880f47348125b8e7d587
Connor Nail <connor.nail@cox.net>
2023-02-10 14:19:00 -0700

    Clan leaderboard endpoint

---
+++ src/pages/api/ClanLeaderboards.js
@@ -0,0 +1,26 @@
+import clientPromise from "../../../lib/mongodb";
+
+export default async function handler(req, res) {
+    try {
+        const client = await clientPromise;
+        const db = client.db("test");
+
+        if (req.method == "GET") {
+            const clansPvECursor = await db.collection("clanScores").find({}, { projection: { _id: false, clanId: true, clanScorePvE: true } }).sort({ clanScorePvE: -1 }).limit(10)
+            const clansPvELeaderboard = await clansPvECursor.toArray()
+
+            const clansPvPCursor = await db.collection("clanScores").find({}, { projection: { _id: false, clanId: true, clanScorePvP: true } }).sort({ clanScorePvP: -1 }).limit(10)
+            const clansPvPLeaderboard = await clansPvPCursor.toArray()
+
+            if (clansPvELeaderboard && clansPvPLeaderboard) {
+                res.status(200).json({ clansPvELeaderboard, clansPvPLeaderboard });
+            } else {
+                res.status(200).json({ error: "There are no clans in the database" })
+            }
+        } else {
+            res.status(500).json({ error: "This endpoint requires a GET request" })
+        }
+    } catch (err) {
+        res.json({ error: "Error" })
+    }
+}
